ba07f83e154eae3dcb23444c4cd3d5c0d52c6ace
brisalchert <brisalchert@gmail.com>
2023-08-10 13:24:01 -0400

    Initial Commit for ReverseInteger.java

---
+++ Problems/Problem7/ReverseInteger.java
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  7. Reverse Integer
+//
+//  Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside
+//  the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+//
+//  Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem7;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+
+    }
+
+    public static int reverse(int x) {
+        // Trivial case for single-digit integer
+        if ((x / 10) == 0) {
+            return x;
+        }
+
+        return x;
+    }
+}
