67b21d2e5b8ea6c9e5970ee536b29c909e461dd7
Brian Salchert <brisalchert@gmail.com>
2023-08-11 15:07:04 -0400

    Added main algorithm

--- Problems/Problem7/ReverseInteger.java
+++ Problems/Problem7/ReverseInteger.java
@@ -9,19 +9,52 @@
 
 package Problem7;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ReverseInteger {
     public static void main(String[] args) {
+        int input = 1234;
 
+        System.out.println(reverse(input));
     }
 
+    /**
+     * Reverses and returns the input integer, or returns 0 if the reversed integer is too large (positive or negative)
+     * @param x the input integer
+     * @return the reversed integer
+     */
     public static int reverse(int x) {
         // Trivial case for single-digit integer
         if ((x / 10) == 0) {
             return x;
         }
 
-        // Add main algorithm here
+        Queue<Integer> digitQueue = new LinkedList<>();
+        int result = 0;
 
-        return x;
+        // Place each digit onto the Stack
+        while (Math.abs(x) > 0) {
+            digitQueue.add(x % 10);
+            x /= 10;
+        }
+
+        // Take each digit off the stack and store in the result
+        while (!digitQueue.isEmpty()) {
+            result += digitQueue.poll();
+
+            // Check if there is a next digit
+            if (!digitQueue.isEmpty()) {
+                // Check if result will be too large (positive or negative)
+                if ((result > (Integer.MAX_VALUE / 10)) || (result < (Integer.MIN_VALUE / 10))) {
+                    return 0;
+                }
+                else {
+                    result *= 10;
+                }
+            }
+        }
+
+        return result;
     }
 }
