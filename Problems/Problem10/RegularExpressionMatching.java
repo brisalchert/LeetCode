9fb2fc322954e6bc1a2264659bcde3d5cea03967
Brian Salchert <brisalchert@gmail.com>
2023-10-14 17:04:59 -0400

    Added Dynamic Programming approach

--- Problems/Problem10/RegularExpressionMatching.java
+++ Problems/Problem10/RegularExpressionMatching.java
@@ -12,25 +12,55 @@
 package Problem10;
 
 public class RegularExpressionMatching {
-    public static void main(String[] args) {
-        String expression = "aa";
-        String pattern = "aa";
+    static Boolean[][] memoTable;
 
-        System.out.println(expressionMatches(expression, pattern));
+    public static void main(String[] args) {
+        String expression = "ab";
+        String pattern = "a*b";
+
+        System.out.println(isMatch(expression, pattern));
     }
 
-    public static boolean expressionMatches(String expression, String pattern) {
-        // Case for special characters in pattern
-        if (pattern.contains("*") || pattern.contains(".")) {
+    public static boolean isMatch(String expression, String pattern) {
+        // Create memoization table for matches
+        memoTable = new Boolean[expression.length() + 1][pattern.length() + 1];
 
+        // Use Dynamic Programming to solve sub-problems
+        return checkMatchDP(0, 0, expression, pattern);
+    }
+
+    public static boolean checkMatchDP(int expressionIndex, int patternIndex, String expression, String pattern) {
+        // Check if memoization table has the result already
+        if (memoTable[expressionIndex][patternIndex] != null) {
+            return memoTable[expressionIndex][patternIndex];
+        }
+
+        boolean result;
+
+        // If pattern is exhausted, then expression is valid if also exhausted
+        if (patternIndex == pattern.length()) {
+            result = (expressionIndex == expression.length());
         }
         else {
-            // Trivial case for no special characters in pattern
-            if (!expression.equals(pattern)) {
-                return false;
+            // Check if current characters match or pattern contains wildcard
+            boolean currentMatch = (expressionIndex < expression.length() &&
+                                        (pattern.charAt(patternIndex) == expression.charAt(expressionIndex) ||
+                                         pattern.charAt(patternIndex) == '.'));
+
+            // If pattern has a '*' at next character, check if 0 instances of current character (skip 2 in pattern)
+            // or 1 or more instances (check current characters and next in expression)
+            if (patternIndex + 1 < pattern.length() && pattern.charAt(patternIndex + 1) == '*') {
+                result = (checkMatchDP(expressionIndex, (patternIndex + 2), expression, pattern) ||
+                          currentMatch && checkMatchDP((expressionIndex + 1), patternIndex, expression, pattern));
+            }
+            else {
+                // If no '*' at next character, check next match in expression and pattern
+                result = currentMatch && checkMatchDP((expressionIndex + 1), (patternIndex + 1), expression, pattern);
             }
         }
 
-        return true;
+        // Set the current location in memoTable to the result
+        memoTable[expressionIndex][patternIndex] = result;
+        return result;
     }
 }
