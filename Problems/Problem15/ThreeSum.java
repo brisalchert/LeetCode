0e6c9b78f1ff64f1d0fc5721f42d79c2821d2282
Brian Salchert <brisalchert@gmail.com>
2024-08-19 16:28:09 -0400

    Implemented faster two-pointer sorted solution

--- Problems/Problem15/ThreeSum.java
+++ Problems/Problem15/ThreeSum.java
@@ -18,28 +18,44 @@ public class ThreeSum {
         int[] nums = {-1, 0, 1, 2, -1, -4};
 
         List<List<Integer>> result = threeSum(nums);
+
         System.out.println(result);
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        // Sort the input so that duplicate triplets will be identical arrays
-        Arrays.sort(nums);
-
         List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        List<Integer> list = new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
+        // Sort the input array
+        Arrays.sort(nums);
 
-                        if (!result.contains(list)) {
-                            result.add(list);
-                        }
-                    }
+        // Iterate through each value for the first index
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            // Use two pointers to check all unique sums
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                // If sum is 0, add to result set
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    // Increment the smaller index, ensuring its value is different from the previous value
+                    do {
+                        j++;
+                    } while (j < k && nums[j] == nums[j - 1]);
+                } else if (sum > 0) {
+                    // If sum is too high, decrement the larger index
+                    k--;
+                } else {
+                    // If sum is too low, increment the smaller index
+                    j++;
                 }
             }
         }
