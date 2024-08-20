b47c209838935851c689e46e33b54289d718d8f7
Brian Salchert <brisalchert@gmail.com>
2024-08-20 08:56:49 -0400

    Added solution using two pointers

--- Problems/Problem16/ThreeSumClosest.java
+++ Problems/Problem16/ThreeSumClosest.java
@@ -11,16 +11,52 @@
 
 package Problem16;
 
+import java.util.Arrays;
+
 public class ThreeSumClosest {
     public static void main(String[] args) {
         int[] nums = {-1, 2, 1, -4};
         int target = 1;
 
+        long start = System.nanoTime();
         System.out.println(threeSumClosest(nums, target));
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1000000 + " ms");
     }
 
     public static int threeSumClosest(int[] nums, int target) {
-        // ADD SOLUTION
-        return 0;
+        int closestSum = nums[0] + nums[1] + nums[2];
+
+        // Sort the input array
+        Arrays.sort(nums);
+
+        // Fix the first index for each iteration
+        for (int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            // Use two pointers to move the sum
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == target) {
+                    // Return if target sum is found
+                    return sum;
+                } else if (sum > target) {
+                    // If sum is too large, decrement larger index
+                    k--;
+                } else {
+                    // If sum is too small, increment smaller index
+                    j++;
+                }
+
+                // Set the new closest sum if needed
+                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+            }
+        }
+
+        return closestSum;
     }
 }
