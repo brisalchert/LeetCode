1b1fb24ab73020ae4c29e43f3ec1c88f40e77ff2
Brian Salchert <brisalchert@gmail.com>
2024-08-23 13:52:41 -0400

    Created LetterCombinationsPhoneNumber.java

---
+++ Problems/Problem17/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,23 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  17. Letter Combinations of a Phone Number
+//
+//  Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could
+//  represent. Return the answer in any order.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationsPhoneNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        return result;
+    }
+}
