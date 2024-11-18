65244007b9469bce407ee174ad902ac507fb3393
Brian Salchert <brisalchert@gmail.com>
2024-11-18 16:04:53 -0500

    Create GenerateParenthese.java

---
+++ Problems/Problem22/GenerateParentheses.java
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  22. Generate Parentheses
+//
+//  Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+//
+//  Example 1:
+//    Input: n = 3
+//    Output: ["((()))","(()())","(())()","()(())","()()()"]
+//
+//  Example 2:
+//    Input: n = 1
+//    Output: ["()"]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem22;
+
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
