c4229f335e1cf9a9c24087549a5e736eadb01a62
Brian Salchert <brisalchert@gmail.com>
2024-11-18 19:53:21 -0500

    Add recursive solution for generating all valid parentheses

--- Problems/Problem22/GenerateParentheses.java
+++ Problems/Problem22/GenerateParentheses.java
@@ -14,14 +14,51 @@
 
 package Problem22;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParentheses {
     public static void main(String[] args) {
+        int n = 3;
 
+        System.out.println(generateParenthesis(n));
     }
 
     public static List<String> generateParenthesis(int n) {
-        return null;
+        // Initialize result list
+        List<String> result = new ArrayList<>();
+
+        // Call the recursive helper method
+        parenthesisHelper(n, result, "", 0, 0);
+
+        return result;
+    }
+
+    /**
+     * Similar to a binary tree traversal, the algorithm checks both possible paths (adding an open or closed
+     * parenthesis) for each current string value. The result list is built from the leaf nodes.
+     * @param n the maximum number of open or closed parentheses
+     * @param result the result list
+     * @param string the current string
+     * @param num_open the number of open parentheses in the current string
+     * @param num_closed the number of closed parentheses in the current string
+     */
+    private static void parenthesisHelper(int n, List<String> result, String string, int num_open, int num_closed) {
+        // Base case: append the string if it is complete
+        if (string.length() == 2 * n) {
+            result.add(string);
+
+            return;
+        }
+
+        // Try to add an open parenthesis and call recursively
+        if (num_open < n) {
+            parenthesisHelper(n, result, string + "(", num_open + 1, num_closed);
+        }
+
+        // Try to add a closed parenthesis and call recursively
+        if (num_closed < num_open) {
+            parenthesisHelper(n, result, string + ")", num_open, num_closed + 1);
+        }
     }
 }
