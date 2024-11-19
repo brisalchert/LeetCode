2bf980f13f9b7d49cf72746cef19f9f367d66313
Brian Salchert <brisalchert@gmail.com>
2024-11-19 09:57:14 -0500

    Add TreeMap solution for merging lists

--- Problems/Problem23/MergeKSortedLists.java
+++ Problems/Problem23/MergeKSortedLists.java
@@ -28,7 +28,7 @@
 
 package Problem23;
 
-import java.util.ArrayList;
+import java.util.TreeMap;
 
 public class MergeKSortedLists {
     public static void main(String[] args) {
@@ -50,8 +50,57 @@ public class MergeKSortedLists {
         }
     }
 
+    /**
+     * Merges k linked lists of integers, where each list is presorted in ascending order.
+     * Utilizes a TreeMap to find the previous node in the merged list for each new node
+     * by finding the last node with equal or lesser value.
+     * @param lists the list of linked lists
+     * @return the merged list
+     */
     public static ListNode mergeKLists(ListNode[] lists) {
-        return null;
+        // Case for empty input
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode result = null;
+
+        // Initialize TreeMap of integers to last node with that value
+        TreeMap<Integer, ListNode> valueMap = new TreeMap<>();
+
+        // Iterate through the list of lists
+        for (ListNode list : lists) {
+            ListNode current = list;
+
+            // Iterate through each node in the inner list
+            while (current != null) {
+                // Save reference to next node in the list
+                ListNode next = current.next;
+
+                // Get the key or closest key below in the TreeMap
+                Integer closestKey = valueMap.floorKey(current.val);
+
+                // If there is no closest key below
+                if (closestKey == null) {
+                    // Insert node at head
+                    current.next = result;
+                    result = current;
+                } else {
+                    // Append the node after the last value less than or equal
+                    ListNode last = valueMap.get(closestKey);
+
+                    current.next = last.next;
+                    last.next = current;
+                }
+
+                // Add the node to the TreeMap
+                valueMap.put(current.val, current);
+
+                current = next;
+            }
+        }
+
+        return result;
     }
 
     public static class ListNode {
