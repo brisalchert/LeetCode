141221d4b9fe630b6817547b256c685da61bd928
Brian Salchert <brisalchert@gmail.com>
2024-11-25 19:36:07 -0500

    Add bitwise shift solution for integer division

--- Problems/Problem29/DivideTwoIntegers.java
+++ Problems/Problem29/DivideTwoIntegers.java
@@ -18,13 +18,54 @@ package Problem29;
 
 public class DivideTwoIntegers {
     public static void main(String[] args) {
-        int dividend = 100;
-        int divisor = 3;
+        int dividend = -2147483648;
+        int divisor = 2;
 
         System.out.println(divide(dividend, divisor));
     }
 
     public static int divide(int dividend, int divisor) {
-        return 0;
+        // Check case for out of bounds quotient
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        // Base cases:
+        if (divisor == 1) {
+            return dividend;
+        }
+
+        if (divisor == -1) {
+            return -dividend;
+        }
+
+        // Determine whether to add or subtract divisor based on signs
+        boolean oppositeSign = (dividend < 0) ^ (divisor < 0);
+
+        // Get the absolute value of both dividend and divisor
+        long absDividend = Math.abs((long) dividend);
+        long absDivisor = Math.abs((long) divisor);
+
+        int quotient = 0;
+
+        // Get quotient by continually finding the largest power of 2 that the dividend is greater or equal to
+        while (absDividend >= absDivisor) {
+            // Set variables for the largest divisor and its multiplier
+            long currentDivisor = absDivisor;
+            int mutliplier = 1;
+
+            // Increase the power of the divisor and multiplier to find the largest possible multiple
+            while (absDividend >= (currentDivisor << 1)) {
+                currentDivisor <<= 1;
+                mutliplier <<= 1;
+            }
+
+            // Subtract the divisor from the dividend and add the multiplier to the quotient
+            absDividend -= currentDivisor;
+            quotient += mutliplier;
+        }
+
+        // Swap the sign of the result if necessary
+        return (oppositeSign ? -quotient : quotient);
     }
 }
