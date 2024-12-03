3b90a9b9649d1ebb9b3305c6f23d7d3bebe3f24e
brisalchert <brisalchert@gmail.com>
2024-12-03 09:47:46 -0500

    Create LongestValidParentheses.java

---
+++ Problems/Problem32/LongestValidParentheses.java
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  32. Longest Valid Parentheses
+//
+//  Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed)
+//  parentheses substring.
+//
+//  Example 1:
+//    Input: s = "(()"
+//    Output: 2
+//    Explanation: The longest valid parentheses substring is "()".
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem32;
+
+public class LongestValidParentheses {
+    public static void main(String[] args) {
+        String input = "(()";
+
+        System.out.println(longestValidParentheses(input));
+    }
+
+    public static int longestValidParentheses(String s) {
+        return 0;
+    }
+}
