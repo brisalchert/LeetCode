9473a2b6e2fd67d057d5e0bb472cc76786e0ade2
brisalchert <brisalchert@gmail.com>
2024-12-10 09:42:30 -0500

    Create SudokuSolver.java

---
+++ Problems/Problem37/SudokuSolver.java
@@ -0,0 +1,43 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  37. Sudoku Solver
+//
+//  Write a program to solve a Sudoku puzzle by filling the empty cells.
+//
+//  A sudoku solution must satisfy all of the following rules:
+//
+//  1. Each of the digits 1-9 must occur exactly once in each row.
+//  2. Each of the digits 1-9 must occur exactly once in each column.
+//  3. Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+//
+//  The '.' character indicates empty cells.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem37;
+
+import java.util.Arrays;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        solveSudoku(board);
+
+        for (char[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    public static void solveSudoku(char[][] board) {
+
+    }
+}
