da397ecd630079babd5b1b7044a543682c5ad019
Brian Salchert <brisalchert@gmail.com>
2024-12-19 12:19:19 -0500

    Create MaxChunksToSort.java

---
+++ Problems/Problem769/MaxChunksToSort.java
@@ -0,0 +1,32 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  769. Max Chunks To Make Sorted
+//
+//  You are given an integer array arr of length n that represents a permutation of the integers in the range
+//  [0, n - 1].
+//
+//  We split arr into some number of chunks (i.e., partitions), and individually sort each chunk. After concatenating
+//  them, the result should equal the sorted array.
+//
+//  Return the largest number of chunks we can make to sort the array.
+//
+//  Example 1:
+//    Input: arr = [4,3,2,1,0]
+//    Output: 1
+//    Explanation:
+//    Splitting into two or more chunks will not return the required result.
+//    For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem769;
+
+public class MaxChunksToSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 1, 0};
+
+        System.out.println(maxChunksToSorted(arr));
+    }
+
+    public static int maxChunksToSorted(int[] arr) {
+        return 0;
+    }
+}
