3caac6658c3c5c19425c8e0eb24536566f11ab83
Brian Salchert <brisalchert@gmail.com>
2024-12-22 09:55:03 -0500

    Create MultiplyStrings.java

---
+++ Problems/Problem43/MultiplyStrings.java
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  43. Multiply Strings
+//
+//  Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2,
+//  also represented as a string.
+//
+//  Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+//
+//  Example 1:
+//    Input: num1 = "2", num2 = "3"
+//    Output: "6"
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem43;
+
+public class MultiplyStrings {
+    public static void main(String[] args) {
+        String num1 = "2", num2 = "3";
+
+        System.out.println(multiply(num1, num2));
+    }
+
+    public static String multiply(String num1, String num2) {
+        return null;
+    }
+}
