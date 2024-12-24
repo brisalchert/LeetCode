14cba0a60d80fc26c18c2ca636663a434cc246cf
Brian Salchert <brisalchert@gmail.com>
2024-12-24 13:22:18 -0500

    Create JumpGame2.java

---
+++ Problems/Problem45/JumpGame2.java
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  45. Jump Game II
+//
+//  You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
+//
+//  Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are
+//  at nums[i], you can jump to any nums[i + j] where:
+//
+//  - 0 <= j <= nums[i] and
+//  - i + j < n
+//
+//  Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated such that you can
+//  reach nums[n - 1].
+//
+//  Example 1:
+//    Input: nums = [2,3,1,1,4]
+//    Output: 2
+//    Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3
+//    steps to the last index.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem45;
+
+public class JumpGame2 {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+
+        System.out.println(jump(nums));
+    }
+
+    public static int jump(int[] nums) {
+        return 1;
+    }
+}
