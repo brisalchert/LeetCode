598ba965fed6fe1dbd6cd5455d94c6bc27ca01fe
Brian Salchert <brisalchert@gmail.com>
2024-12-26 18:04:26 -0500

    Create Permutations.java

---
+++ Problems/Problem46/Permutations.java
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  46. Permutations
+//
+//  Given an array nums of distinct integers, return all the possible permutations. You can return the answer
+//  in any order.
+//
+//  Example 1:
+//    Input: nums = [1,2,3]
+//    Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem46;
+
+import java.util.List;
+
+public class Permutations {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        System.out.println(permute(nums));
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        return null;
+    }
+}
