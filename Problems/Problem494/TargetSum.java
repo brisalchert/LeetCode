03e17efcaf7024c2f5e26b642e968f437b3a1ee8
Brian Salchert <brisalchert@gmail.com>
2024-12-25 21:56:55 -0500

    Add tabulation DP solution for faster runtime

--- Problems/Problem494/TargetSum.java
+++ Problems/Problem494/TargetSum.java
@@ -33,17 +33,42 @@ public class TargetSum {
     }
 
     public static int findTargetSumWays(int[] nums, int target) {
-        return findTargetHelper(nums, target, 0, 0);
-    }
+        // Get total sum of array
+        int sum = 0;
 
-    private static int findTargetHelper(int[] nums, int target, int index, int sum) {
-        if (index == nums.length) {
-            return sum == target ? 1 : 0;
+        for (int num : nums) {
+            sum += num;
         }
 
-        int positive = findTargetHelper(nums, target, index + 1, sum + nums[index]);
-        int negative = findTargetHelper(nums, target, index + 1, sum - nums[index]);
+        // Check if target impossible
+        if ((sum - target) % 2 == 1 || target > sum) {
+            return 0;
+        }
 
-        return positive + negative;
+        // Calculate amount to negate ((sum - target) / 2)
+        int negateSum = (sum - target) / 2;
+
+        // Create Tabulation array
+        int[][] tab = new int[nums.length + 1][negateSum + 1];
+        tab[0][0] = 1;
+
+        // Tabulate num combinations for each number of elements
+        for (int numElements = 1; numElements <= nums.length; numElements++) {
+            // Tabulate num combinations for each possible sum between 0 and negateSum
+            for (int targetSum = 0; targetSum <= negateSum; targetSum++) {
+                // Check if current value can contribute to negateSum
+                if (nums[numElements - 1] <= targetSum) {
+                    // Set num solutions to previous solutions + new solutions
+                    tab[numElements][targetSum] = tab[numElements - 1][targetSum]
+                            + tab[numElements - 1][targetSum - nums[numElements - 1]];
+                } else {
+                    // Set num solutions to previous solutions
+                    tab[numElements][targetSum] = tab[numElements - 1][targetSum];
+                }
+            }
+        }
+
+        // Return number of valid solutions
+        return tab[nums.length][negateSum];
     }
 }
