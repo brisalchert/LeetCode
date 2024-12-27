ca19524382b6aa26b94d2e0ddb817a9b90530666
Brian Salchert <brisalchert@gmail.com>
2024-12-27 13:09:51 -0500

    Add sorting solution with duplicate checking

--- Problems/Problem47/Permutations2.java
+++ Problems/Problem47/Permutations2.java
@@ -14,6 +14,8 @@
 
 package Problem47;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations2 {
@@ -24,6 +26,47 @@ public class Permutations2 {
     }
 
     public static List<List<Integer>> permuteUnique(int[] nums) {
-        return null;
+        // Sort so duplicates are adjacent
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> permutation = new ArrayList<>();
+        List<Boolean> visited = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            visited.add(false);
+        }
+
+        permuteUniqueHelper(nums, visited, permutation, result);
+
+        return result;
+    }
+
+    private static void permuteUniqueHelper(int[] nums, List<Boolean> visited, List<Integer> permutation,
+                                            List<List<Integer>> result) {
+        // Base case: if permutation is complete, add it to the output
+        if (permutation.size() == nums.length) {
+            result.add(new ArrayList<>(permutation));
+
+            return;
+        }
+
+        // Recursive case: Add each unvisited value to the permutation,
+        // skipping duplicates in the same recursion level
+        for (int i = 0; i < nums.length; i++) {
+            if (visited.get(i)) {
+                continue;
+            }
+
+            // Skip duplicates within current recursion level
+            if (i > 0 && nums[i] == nums[i - 1] && !visited.get(i - 1)) {
+                continue;
+            }
+
+            permutation.add(nums[i]);
+            visited.set(i, true);
+            permuteUniqueHelper(nums, visited, permutation, result);
+            permutation.remove(permutation.size() - 1);
+            visited.set(i, false);
+        }
     }
 }
