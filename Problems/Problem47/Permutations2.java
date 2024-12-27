ba794ded6ec046ae8ce23f2541e6056986896cbc
Brian Salchert <brisalchert@gmail.com>
2024-12-27 12:18:44 -0500

    Create Permutations2.java

---
+++ Problems/Problem47/Permutations2.java
@@ -0,0 +1,29 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  47. Permutations II
+//
+//  Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations
+//  in any order.
+//
+//  Example 1:
+//    Input: nums = [1,1,2]
+//    Output:
+//    [[1,1,2],
+//     [1,2,1],
+//     [2,1,1]]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem47;
+
+import java.util.List;
+
+public class Permutations2 {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2};
+
+        System.out.println(permuteUnique(nums));
+    }
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        return null;
+    }
+}
