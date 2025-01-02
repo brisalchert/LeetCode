de3b3840e3f72ab90fb456b8c519a07095e442f4
Brian Salchert <brisalchert@gmail.com>
2025-01-02 11:25:13 -0500

    Add binary exponentiation solution

--- Problems/Problem50/Pow.java
+++ Problems/Problem50/Pow.java
@@ -12,17 +12,39 @@ package Problem50;
 
 public class Pow {
     public static void main(String[] args) {
-        double x = 2.0;
-        int n = 10;
+        double x = 1.000000001;
+        int n = 2147483647;
 
         System.out.println(myPow(x, n));
     }
 
     public static double myPow(double x, int n) {
-        if (n == 0) {
+        long exp = n;
+
+        if (exp == 0) {
             return 1;
         }
 
-        return 0;
+        double result = 1.0;
+
+        // Handle negative exponent
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        // Calculate result with binary exponentiation
+        while (exp > 0) {
+            // If n is odd, multiply once so n is now even
+            if (exp % 2 == 1) {
+                result *= x;
+            }
+
+            // x^n == (x^2)^(n/2)
+            x *= x;
+            exp /= 2;
+        }
+
+        return result;
     }
 }
