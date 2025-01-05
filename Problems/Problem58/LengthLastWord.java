522e2a664770d55af08266eab26f9a8157500e00
Brian Salchert <brisalchert@gmail.com>
2025-01-05 14:41:49 -0500

    Create LengthLastWord.java

---
+++ Problems/Problem58/LengthLastWord.java
@@ -0,0 +1,26 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  58. Length of Last Word
+//
+//  Given a string s consisting of words and spaces, return the length of the last word in the string.
+//
+//  A word is a maximal substring consisting of non-space characters only.
+//
+//  Example 1:
+//    Input: s = "Hello World"
+//    Output: 5
+//    Explanation: The last word is "World" with length 5.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem58;
+
+public class LengthLastWord {
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        return 0;
+    }
+}
