b9bc8725aee69d77a30b3dbfb8c75894a2989dca
Brian Salchert <brisalchert@gmail.com>
2025-01-10 20:24:36 -0500

    Add initial solution with recursive backtracking

--- Problems/Problem51/NQueens.java
+++ Problems/Problem51/NQueens.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class NQueens {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 1;
 
         System.out.println(solveNQueens(n));
     }
@@ -30,6 +30,58 @@ public class NQueens {
     public static List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
 
+        nQueensHelper(n, 0, new ArrayList<>(), -2, new ArrayList<>(), result);
+
         return result;
+    }
+
+    private static void nQueensHelper(int n, int row, List<Integer> columnSet, int lastColumn,
+                                      List<String> current, List<List<String>> result) {
+        // Base case: If all rows filled, valid solution is found
+        if (row == n) {
+            result.add(new ArrayList<>(current));
+
+            return;
+        }
+
+        // Recursive case: Try to place a queen in each valid spot in the next row
+        for (int j = 0; j < n; j++) {
+            boolean diagonal = false;
+
+            // Check diagonals
+            for (int i = 0; i < row - 1; i++) {
+                if (row - i == Math.abs(columnSet.get(i) - j)) {
+                    diagonal = true;
+                    break;
+                }
+            }
+
+            if (diagonal) {
+                continue;
+            }
+
+            if (!columnSet.contains(j) && Math.abs(lastColumn - j) >= 2) {
+                // Place queen and continue recursively
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < n; i++) {
+                    if (i == j) {
+                        sb.append('Q');
+                    } else {
+                        sb.append('.');
+                    }
+                }
+
+                current.add(sb.toString());
+
+                ArrayList<Integer> nextColumnSet = new ArrayList<>(columnSet);
+                nextColumnSet.add(j);
+
+                nQueensHelper(n, row + 1, nextColumnSet, j, current, result);
+
+                // Reset row and try remaining columns
+                current.remove(current.size() - 1);
+            }
+        }
     }
 }
