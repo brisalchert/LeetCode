83740a18116b29b4199129c1a129024bd03c039d
Brian Salchert <brisalchert@gmail.com>
2025-01-10 21:09:36 -0500

    Add faster algorithm recognizing mirrored solutions

--- Problems/Problem51/NQueens.java
+++ Problems/Problem51/NQueens.java
@@ -18,39 +18,53 @@
 package Problem51;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class NQueens {
     public static void main(String[] args) {
-        int n = 1;
+        int n = 4;
 
         System.out.println(solveNQueens(n));
     }
 
     public static List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
+        boolean[] columnsUsed = new boolean[n];
+        int[] columnValues = new int[n];
+        int mid = (n + 1) / 2;
 
-        nQueensHelper(n, 0, new ArrayList<>(), -2, new ArrayList<>(), result);
+        // Only find solutions for the left half of the board (including
+        // center): other solutions will be mirrored
+        for (int j = 0; j < mid; j++) {
+            columnValues[0] = j;
+            columnsUsed[j] = true;
+            nQueensDFS(n, 1, columnsUsed, columnValues, result);
+            columnsUsed[j] = false;
+        }
 
         return result;
     }
 
-    private static void nQueensHelper(int n, int row, List<Integer> columnSet, int lastColumn,
-                                      List<String> current, List<List<String>> result) {
-        // Base case: If all rows filled, valid solution is found
+    private static void nQueensDFS(int n, int row, boolean[] columnsUsed,
+                                   int[] columnValues, List<List<String>> result) {
         if (row == n) {
-            result.add(new ArrayList<>(current));
+            // Add solution
+            addResult(n, columnValues, result);
 
             return;
         }
 
-        // Recursive case: Try to place a queen in each valid spot in the next row
+        // Try to place a queen in each column for the current row
         for (int j = 0; j < n; j++) {
-            boolean diagonal = false;
+            if (columnsUsed[j]) {
+                continue;
+            }
 
-            // Check diagonals
-            for (int i = 0; i < row - 1; i++) {
-                if (row - i == Math.abs(columnSet.get(i) - j)) {
+            // Check diagonals with previous rows
+            boolean diagonal = false;
+            for (int r = 0; r < row; r++) {
+                if (row - r == Math.abs(columnValues[r] - j)) {
                     diagonal = true;
                     break;
                 }
@@ -60,28 +74,40 @@ public class NQueens {
                 continue;
             }
 
-            if (!columnSet.contains(j) && Math.abs(lastColumn - j) >= 2) {
-                // Place queen and continue recursively
-                StringBuilder sb = new StringBuilder();
-
-                for (int i = 0; i < n; i++) {
-                    if (i == j) {
-                        sb.append('Q');
-                    } else {
-                        sb.append('.');
-                    }
-                }
-
-                current.add(sb.toString());
-
-                ArrayList<Integer> nextColumnSet = new ArrayList<>(columnSet);
-                nextColumnSet.add(j);
-
-                nQueensHelper(n, row + 1, nextColumnSet, j, current, result);
-
-                // Reset row and try remaining columns
-                current.remove(current.size() - 1);
-            }
+            columnsUsed[j] = true;
+            columnValues[row] = j;
+            nQueensDFS(n, row + 1, columnsUsed, columnValues, result);
+            columnsUsed[j] = false;
         }
+    }
+
+    private static void addResult(int n, int[] columns, List<List<String>> result) {
+        List<String> solution = new ArrayList<>();
+        char[] currentRow = new char[n];
+        Arrays.fill(currentRow, '.');
+
+        for (int col : columns) {
+            currentRow[col] = 'Q';
+            solution.add(new String(currentRow));
+            currentRow[col] = '.';
+        }
+
+        result.add(solution);
+
+        // If n is odd and the first queen is in the middle column,
+        // mirror will be identical
+        if (n % 2 == 1 && columns[0] == (n / 2)) {
+            return;
+        }
+
+        // Add mirrored solution to result
+        List<String> mirrorSolution = new ArrayList<>();
+
+        for (String row : solution) {
+            String reverseRow = new StringBuilder(row).reverse().toString();
+            mirrorSolution.add(reverseRow);
+        }
+
+        result.add(mirrorSolution);
     }
 }
