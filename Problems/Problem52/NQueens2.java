3252e8751be9e2ffdf607c797e53e239bebcd761
Brian Salchert <brisalchert@gmail.com>
2025-01-11 20:57:38 -0500

    Create NQueens2.java

---
+++ Problems/Problem52/NQueens2.java
@@ -0,0 +1,27 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  52. N-Queens II
+//
+//  The n-queens puzzle is the problem of placing n queens on an n x n chessboard such that no two queens attack
+//  each other.
+//
+//  Given an integer n, return the number of distinct solutions to the n-queens puzzle.
+//
+//  Example 1:
+//    Input: n = 4
+//    Output: 2
+//    Explanation: There are two distinct solutions to the 4-queens puzzle as shown.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem52;
+
+public class NQueens2 {
+    public static void main(String[] args) {
+        int n = 4;
+
+        System.out.println(totalNQueens(n));
+    }
+
+    public static int totalNQueens(int n) {
+        return 0;
+    }
+}
