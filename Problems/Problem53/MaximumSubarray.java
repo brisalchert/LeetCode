ecd62f3e8f4fbcfc7e2dbabfc72588b35077d5e0
Brian Salchert <brisalchert@gmail.com>
2025-01-12 17:29:23 -0500

    Create MaximumSubarray.java

---
+++ Problems/Problem53/MaximumSubarray.java
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  53. Maximum Subarray
+//
+//  Given an integer array nums, find the subarray with the largest sum, and return its sum.
+//
+//  Example 1:
+//    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//    Output: 6
+//    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem53;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println(maxSubArray(nums));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        return 0;
+    }
+}
