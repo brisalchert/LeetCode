331bf6201e045016f92f8cd44fc8a11eb4bc2d87
Brian Salchert <brisalchert@gmail.com>
2025-01-13 16:59:07 -0500

    Add iterative solution using level traversal

--- Problems/Problem54/SpiralMatrix.java
+++ Problems/Problem54/SpiralMatrix.java
@@ -10,6 +10,7 @@
 
 package Problem54;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralMatrix {
@@ -24,6 +25,45 @@ public class SpiralMatrix {
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        List<Integer> result = new ArrayList<>();
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int level = 0;
+
+        while (level < (Math.min(m + 1, n + 1) / 2)) {
+            int i = level;
+            int j = level;
+
+            // Always add first element of a given level
+            result.add(matrix[i][j]);
+
+            while (j < n - level - 1) {
+                j++;
+                result.add(matrix[i][j]);
+            }
+
+            while (i < m - level - 1) {
+                i++;
+                result.add(matrix[i][j]);
+            }
+
+            if (level < (m / 2)) {
+                while (j > level) {
+                    j--;
+                    result.add(matrix[i][j]);
+                }
+            }
+
+            if (level < (n / 2)) {
+                while (i > level + 1) {
+                    i--;
+                    result.add(matrix[i][j]);
+                }
+            }
+
+            level++;
+        }
+
+        return result;
     }
 }
