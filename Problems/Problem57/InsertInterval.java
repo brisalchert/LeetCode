8ab7e1f882b0d200fcbd2eb5a6bed8affd79a3f7
Brian Salchert <brisalchert@gmail.com>
2025-01-22 15:52:28 -0500

    Add ArrayList binary search solution

--- Problems/Problem57/InsertInterval.java
+++ Problems/Problem57/InsertInterval.java
@@ -19,21 +19,68 @@
 
 package Problem57;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class InsertInterval {
     public static void main(String[] args) {
         int[][] intervals = {
-                {1, 3},
-                {6, 9}
+                {1, 2},
+                {3, 5},
+                {6, 7},
+                {8, 10},
+                {12, 16}
         };
 
-        int[] newInterval = {2, 5};
+        int[] newInterval = {4, 8};
 
         System.out.println(Arrays.deepToString(insert(intervals, newInterval)));
     }
 
     public static int[][] insert(int[][] intervals, int[] newInterval) {
-        return null;
+        ArrayList<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals));
+
+        // Get insertion index
+        int insertIndex = binarySearchInterval(intervalList, newInterval, 0, intervals.length - 1);
+
+        // Check if it is a merge or an insert
+        if (insertIndex < intervalList.size() &&
+                intervalList.get(insertIndex)[1] >= newInterval[0] &&
+                intervalList.get(insertIndex)[0] <= newInterval[0]) {
+            intervalList.get(insertIndex)[1] = Math.max(intervalList.get(insertIndex)[1], newInterval[1]);
+        } else {
+            intervalList.add(insertIndex, newInterval);
+        }
+
+        // Check if the new interval can be merged with greater intervals
+        while (insertIndex + 1 < intervalList.size() &&
+                intervalList.get(insertIndex)[1] >= intervalList.get(insertIndex + 1)[0]) {
+            intervalList.get(insertIndex)[1] = Math.max(intervalList.get(insertIndex + 1)[1], intervalList.get(insertIndex)[1]);
+
+            intervalList.remove(insertIndex + 1);
+        }
+
+        return intervalList.toArray(new int[intervalList.size()][]);
+    }
+
+    private static int binarySearchInterval(ArrayList<int[]> intervals, int[] newInterval, int low, int high) {
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            // Check if newInterval[start] fits in the interval at mid
+            if (intervals.get(mid)[1] >= newInterval[0] && intervals.get(mid)[0] <= newInterval[0]) {
+                return mid;
+            }
+
+            // Check if newInterval is entirely smaller than mid
+            if (newInterval[0] < intervals.get(mid)[1]) {
+                return binarySearchInterval(intervals, newInterval, low, mid - 1);
+            }
+
+            return binarySearchInterval(intervals, newInterval, mid + 1, high);
+        }
+
+        // Return index where insertion should occur
+        return low;
     }
 }
