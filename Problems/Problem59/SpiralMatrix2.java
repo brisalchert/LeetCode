9bd55b11b4acff628bce88228e677f4266fb382e
Brian Salchert <brisalchert@gmail.com>
2025-01-26 17:20:35 -0500

    Create SpiralMatrix2.java

---
+++ Problems/Problem59/SpiralMatrix2.java
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  59. Spiral Matrix II
+//
+//  Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+//
+//  Example 1:
+//    Input: n = 3
+//    Output: [[1,2,3],[8,9,4],[7,6,5]]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem59;
+
+import java.util.Arrays;
+
+public class SpiralMatrix2 {
+    public static void main(String[] args) {
+        int n = 3;
+
+        System.out.println(Arrays.deepToString(generateMatrix(n)));
+    }
+
+    public static int[][] generateMatrix(int n) {
+        return null;
+    }
+}
