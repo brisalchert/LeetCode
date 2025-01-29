7af7d4192200dfa8e00b967be3cbf5950b2f8e6e
Brian Salchert <brisalchert@gmail.com>
2025-01-28 20:43:57 -0500

    Add iterative 0ms solution

--- Problems/Problem66/PlusOne.java
+++ Problems/Problem66/PlusOne.java
@@ -21,12 +21,24 @@ import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
-        int[] digits = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] digits = {9, 9, 9, 9};
 
-        System.out.println(Arrays.toString(digits));
+        System.out.println(Arrays.toString(plusOne(digits)));
     }
 
     public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        // All preceding digits are 0
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
         return digits;
     }
 }
