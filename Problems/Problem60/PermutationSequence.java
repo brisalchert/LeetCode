7c9af9dc5e4478bc61d8ffa7f2a5310e95804bf3
Brian Salchert <brisalchert@gmail.com>
2025-01-30 20:38:37 -0500

    Add 1ms runtime solution using factorial indexing

--- Problems/Problem60/PermutationSequence.java
+++ Problems/Problem60/PermutationSequence.java
@@ -21,6 +21,9 @@
 
 package Problem60;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PermutationSequence {
     public static void main(String[] args) {
         int n = 4;
@@ -30,6 +33,33 @@ public class PermutationSequence {
     }
 
     public static String getPermutation(int n, int k) {
-        return "";
+        StringBuilder sb = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            numbers.add(i);
+        }
+
+        k--; // For indexing
+
+        for (int i = 1; i <= n; i++) {
+            int index = k / factorial(n - i);
+            sb.append(numbers.get(index));
+            numbers.remove(index);
+            k -= index * factorial(n - i);
+        }
+
+        return sb.toString();
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+
+        int result = n;
+
+        for (int i = 1; i < n - 1; i++) {
+            result *= (n - i);
+        }
+
+        return result;
     }
 }
