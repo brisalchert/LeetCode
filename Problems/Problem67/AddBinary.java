90d08e047c83c4bcb40d25f1eea3a0c98fb79102
Brian Salchert <brisalchert@gmail.com>
2025-02-10 18:13:12 -0500

    Create AddBinary.java

---
+++ Problems/Problem67/AddBinary.java
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  67. Add Binary
+//
+//  Given two binary strings a and b, return their sum as a binary string.
+//
+//  Example 1:
+//    Input: a = "11", b = "1"
+//    Output: "100"
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem67;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+        System.out.println(addBinary(a, b));
+    }
+
+    public static String addBinary(String a, String b) {
+        return "";
+    }
+}
