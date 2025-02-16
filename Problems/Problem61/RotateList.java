185887db7be4ef5cf8d5469ed703d321bdf36c1c
Brian Salchert <brisalchert@gmail.com>
2025-02-16 17:31:13 -0500

    Create RotateList.java

---
+++ Problems/Problem61/RotateList.java
@@ -0,0 +1,42 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  61. Rotate List
+//
+//  Given the head of a linked list, rotate the list to the right by k places.
+//
+//  Example 1:
+//    Input: head = [1,2,3,4,5], k = 2
+//    Output: [4,5,1,2,3]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem61;
+
+public class RotateList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode current = head;
+
+        for (int i = 0; i < 4; i++) {
+            current.next = new ListNode(i + 2);
+            current = current.next;
+        }
+
+        head = rotateRight(head, 2);
+
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
