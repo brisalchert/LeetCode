52aa959db04938cfeda7e41d62a0bbf07a10ebd9
Brian Salchert <brisalchert@gmail.com>
2025-02-20 20:30:40 -0500

    Create UniquePaths2.java

---
+++ Problems/Problem63/UniquePaths2.java
@@ -0,0 +1,40 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  63. Unique Paths II
+//
+//  You are given an m x n integer array grid. There is a robot initially located in the top-left corner
+//  (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot
+//  can only move either down or right at any point in time.
+//
+//  An obstacle and space are marked as 1 or 0 respectively in grid. A path that the robot takes cannot include any
+//  square that is an obstacle.
+//
+//  Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+//
+//  The testcases are generated so that the answer will be less than or equal to 2 * 10^9.
+//
+//  Example 1:
+//    Input: obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+//    Output: 2
+//    Explanation: There is one obstacle in the middle of the 3x3 grid above.
+//    There are two ways to reach the bottom-right corner:
+//    1. Right -> Right -> Down -> Down
+//    2. Down -> Down -> Right -> Right
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem63;
+
+public class UniquePaths2 {
+    public static void main(String[] args) {
+        int[][] obstacleGrid = {
+                {0, 0, 0},
+                {0, 1, 0},
+                {0, 0, 0},
+        };
+
+        System.out.println(uniquePathsWithObstacles(obstacleGrid));
+    }
+
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        return 0;
+    }
+}
