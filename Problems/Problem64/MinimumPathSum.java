f6670d42912987c6122f5e0b43729419e5ab4fc1
Brian Salchert <brisalchert@gmail.com>
2025-02-22 21:24:01 -0500

    Create MinimumPathSum.java

---
+++ Problems/Problem64/MinimumPathSum.java
@@ -0,0 +1,31 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  64. Minimum Path Sum
+//
+//  Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes
+//  the sum of all numbers along its path.
+//
+//  Note: You can only move either down or right at any point in time.
+//
+//  Example 1:
+//    Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+//    Output: 7
+//    Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem64;
+
+public class MinimumPathSum {
+    public static void main(String[] args) {
+        int[][] grid = {
+                {1,3,1},
+                {1,5,1},
+                {4,2,1}
+        };
+
+        System.out.println(minPathSum(grid));
+    }
+
+    public static int minPathSum(int[][] grid) {
+        return 0;
+    }
+}
