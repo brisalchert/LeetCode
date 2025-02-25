add75ee72e0ad2967f9dcf2d08e69fd2cb85c0a5
Brian Salchert <brisalchert@gmail.com>
2025-02-24 20:13:48 -0500

    Add initial iterative solution

--- Problems/Problem68/TextJustification.java
+++ Problems/Problem68/TextJustification.java
@@ -35,8 +35,8 @@ import java.util.List;
 
 public class TextJustification {
     public static void main(String[] args) {
-        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
-        int maxWidth = 16;
+        String[] words = {"a"};
+        int maxWidth = 1;
 
         List<String> result = fullJustify(words, maxWidth);
 
@@ -45,6 +45,74 @@ public class TextJustification {
         }
     }
     public static List<String> fullJustify(String[] words, int maxWidth) {
-        return new ArrayList<>();
+        List<String> justified = new ArrayList<>();
+        int wordIndex = 0;
+        int lineStart = 0;
+        int lineLength = 0;
+
+        while (wordIndex < words.length) {
+            int currentLength = words[wordIndex].length();
+
+            if (wordIndex == lineStart) {
+                lineLength += currentLength;
+                wordIndex++;
+            } else if (maxWidth - lineLength > currentLength + 1) {
+                lineLength += currentLength + 1;
+                wordIndex++;
+            } else if (maxWidth - lineLength == currentLength + 1) {
+                // Add all words and append next line
+                String line = buildLine(words, lineStart, wordIndex);
+                justified.add(line);
+                lineLength = 0;
+                lineStart = ++wordIndex;
+            } else {
+                // Justify next line and append
+                int spaces = maxWidth - lineLength;
+                int wordOffset = 0;
+                int numWords = Math.max(wordIndex - lineStart - 1, 1);
+
+                while (spaces > 0) {
+                    words[lineStart + wordOffset] += " ";
+                    wordOffset = (wordOffset + 1) % numWords;
+                    spaces--;
+                }
+
+                // Add all words and append next line
+                String line = buildLine(words, lineStart, wordIndex - 1);
+                justified.add(line);
+                lineLength = 0;
+                lineStart = wordIndex;
+            }
+        }
+
+        // Add remaining words
+        if (lineStart < words.length) {
+            StringBuilder lastLine = new StringBuilder();
+            lastLine.append(words[lineStart++]);
+
+            while (lineStart < words.length) {
+                lastLine.append(" ").append(words[lineStart++]);
+            }
+
+            while (lastLine.length() < maxWidth) {
+                lastLine.append(" ");
+            }
+
+            justified.add(lastLine.toString());
+        }
+
+        return justified;
+    }
+
+    private static String buildLine(String[] words, int lineStart, int lineEnd) {
+        StringBuilder line = new StringBuilder();
+
+        while (lineStart < lineEnd) {
+            line.append(words[lineStart++]).append(" ");
+        }
+
+        line.append(words[lineStart]);
+
+        return line.toString();
     }
 }
