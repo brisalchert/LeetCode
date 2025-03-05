988c00bbba49bd0e23bfa8dc0a67e4fed29c1c89
Brian Salchert <brisalchert@gmail.com>
2025-03-04 21:22:43 -0500

    Add single array space-optimized solution

--- Problems/Problem72/EditDistance.java
+++ Problems/Problem72/EditDistance.java
@@ -34,27 +34,29 @@ public class EditDistance {
         int m = word1.length();
         int n = word2.length();
 
-        int[] previousRow = new int[n + 1];
+        int previous;
         int[] currentRow = new int[n + 1];
 
         // Initialize first row with base case values
         for (int j = 0; j <= n; j++) {
-            previousRow[j] = j;
+            currentRow[j] = j;
         }
 
         // Compute remaining matrix values
         for (int i = 1; i <= m; i++) {
             // Initialize first column with row value
+            previous = currentRow[0];
             currentRow[0] = i;
 
             for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) currentRow[j] = previousRow[j - 1]; // Equal
-                else currentRow[j] = 1 + Math.min(previousRow[j - 1], Math.min(previousRow[j], currentRow[j - 1]));
-            }
+                int temp = currentRow[j];
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) currentRow[j] = previous; // Equal
+                else currentRow[j] = 1 + Math.min(previous, Math.min(currentRow[j], currentRow[j - 1]));
 
-            previousRow = Arrays.copyOf(currentRow, currentRow.length);
+                previous = temp; // Save diagonal value from previous row
+            }
         }
 
-        return previousRow[n];
+        return currentRow[n];
     }
 }
