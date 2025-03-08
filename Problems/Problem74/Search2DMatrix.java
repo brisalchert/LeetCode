696c28a201228fe1de57e241bb553f764f4b88a4
Brian Salchert <brisalchert@gmail.com>
2025-03-07 19:43:52 -0500

    Add double binary search solution

--- Problems/Problem74/Search2DMatrix.java
+++ Problems/Problem74/Search2DMatrix.java
@@ -31,6 +31,45 @@ public class Search2DMatrix {
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m - 1;
+
+        // Perform binary search on first column
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (matrix[mid][0] == target) {
+                return true;
+            }
+
+            if (matrix[mid][0] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        if (right < 0) return false; // Target does not exist if first value in matrix is greater than it
+
+        int rowIndex = right;
+        left = 0;
+        right = n - 1;
+
+        // Perform binary search on relevant row
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (matrix[rowIndex][mid] == target) {
+                return true;
+            }
+
+            if (matrix[rowIndex][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
         return false;
     }
 }
