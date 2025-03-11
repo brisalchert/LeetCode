e19991cbec61db6429578908123013b3a8c8b91e
Brian Salchert <brisalchert@gmail.com>
2025-03-11 12:00:19 -0400

    Create Combinations.java

---
+++ Problems/Problem77/Combinations.java
@@ -0,0 +1,33 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  77. Combinations
+//
+//  Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+//
+//  You may return the answer in any order.
+//
+//  Example 1:
+//    Input: n = 4, k = 2
+//    Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+//    Explanation: There are 4 choose 2 = 6 total combinations.
+//    Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem77;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 2;
+
+        System.out.println(combine(n, k));
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+}
