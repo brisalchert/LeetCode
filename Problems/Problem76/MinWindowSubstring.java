9533576731f991983294179faf71fbeb78fdd10e
Brian Salchert <brisalchert@gmail.com>
2025-03-10 21:12:26 -0400

    Add character mapping countdown solution

--- Problems/Problem76/MinWindowSubstring.java
+++ Problems/Problem76/MinWindowSubstring.java
@@ -24,8 +24,48 @@ public class MinWindowSubstring {
     }
 
     public static String minWindow(String s, String t) {
-        if (t.length() > s.length()) return "";
+        if (s == null || t == null || s.isEmpty() || t.isEmpty() || s.length() < t.length()) return "";
 
-        return "";
+        // Map for ASCII character indices
+        int[] charMap = new int[128];
+        int count = t.length();
+        int left = 0, right = 0, minLength = Integer.MAX_VALUE, resultIndex = 0;
+
+        // Add character counts in t to charMap
+        for (char c : t.toCharArray()) {
+            charMap[c]++;
+        }
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+
+            // Decrement the count if the character is necessary for the substring
+            if (charMap[c] > 0) {
+                count--;
+            }
+
+            charMap[c]--;
+
+            // Check length of valid substrings
+            while (count == 0) {
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    resultIndex = left;
+                }
+
+                // Add character back to map if it is part of t
+                if (charMap[s.charAt(left)] == 0) {
+                    count++;
+                }
+
+                charMap[s.charAt(left)]++;
+                left++;
+            }
+
+            right++;
+        }
+
+        // Return an empty string if no valid substring was found
+        return (minLength == Integer.MAX_VALUE) ? "" : s.substring(resultIndex, resultIndex + minLength);
     }
 }
