fc0cf5187083653dd6748e9c64ea84584ee33c31
Brian Salchert <brisalchert@gmail.com>
2025-04-07 23:16:02 -0400

    Add DFS solution for finding words

--- Problems/Problem79/WordSearch.java
+++ Problems/Problem79/WordSearch.java
@@ -26,6 +26,44 @@ public class WordSearch {
     }
 
     public static boolean exist(char[][] board, String word) {
+        // Iterate through the board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                // Start a DFS when the first character is found
+                if (board[i][j] == word.charAt(0)) {
+                    if (existDFS(board, word, i, j, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean existDFS(char[][] board, String word, int i, int j, int index) {
+        // Base cases
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+
+        // Recursive case
+        board[i][j] = ' ';
+
+        // Check for next character in adjacent cells
+        if (existDFS(board, word, i + 1, j, index + 1) ||
+            existDFS(board, word, i - 1, j, index + 1) ||
+            existDFS(board, word, i, j + 1, index + 1) ||
+            existDFS(board, word, i, j - 1, index + 1)) {
+            return true;
+        }
+
+        // Return false for a failed existence check
+        board[i][j] = word.charAt(index);
         return false;
     }
 }
