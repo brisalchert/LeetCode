56e3022ae2dde4f4599c4646db7ea3906ebb9944
Brian Salchert <brisalchert@gmail.com>
2025-04-21 19:32:42 -0400

    Create RemoveDuplicatesSortedList2.java

---
+++ Problems/Problem82/RemoveDuplicatesSortedList2.java
@@ -0,0 +1,59 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  82. Remove Duplicates from Sorted List II
+//
+//  Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct
+//  numbers from the original list. Return the linked list sorted as well.
+//
+//  Example 1:
+//    Input: head = [1,2,3,3,4,4,5]
+//    Output: [1,2,5]
+//
+//  Example 2:
+//    Input: head = [1,1,1,2,3]
+//    Output: [2,3]
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem82;
+
+public class RemoveDuplicatesSortedList2 {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 3, 4, 4, 5};
+        ListNode head = new ListNode(values[0]);
+        ListNode current = head;
+
+        for (int i = 1; i < values.length; i++) {
+            current.next = new ListNode(values[i]);
+            current = current.next;
+        }
+
+        head = deleteDuplicates(head);
+        current = head;
+
+        System.out.print("[");
+
+        while (current != null) {
+            System.out.print(current.val);
+
+            if (current.next != null) {
+                System.out.print(", ");
+            }
+
+            current = current.next;
+        }
+
+        System.out.println("]");
+    }
+
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
