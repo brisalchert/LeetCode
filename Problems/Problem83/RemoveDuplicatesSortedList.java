a0851f47e184d976ec0ae83b8cb321d43328598b
Brian Salchert <brisalchert@gmail.com>
2025-04-22 19:14:53 -0400

    Add single pointer solution

--- Problems/Problem83/RemoveDuplicatesSortedList.java
+++ Problems/Problem83/RemoveDuplicatesSortedList.java
@@ -45,6 +45,20 @@ public class RemoveDuplicatesSortedList {
     }
 
     public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode current = head;
+
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
         return head;
     }
 
