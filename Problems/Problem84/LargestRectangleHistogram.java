1d4151a4bbd4929c1b67f831bdac563caa8516b2
Brian Salchert <brisalchert@gmail.com>
2025-04-23 19:32:58 -0400

    Create LargestRectangleHistogram.java

---
+++ Problems/Problem84/LargestRectangleHistogram.java
@@ -0,0 +1,28 @@
+//----------------------------------------------------------------------------------------------------------------------
+//  84. Largest Rectangle in Histogram
+//
+//  Given an array of integers "heights" representing the histogram's bar height where the width of each bar is 1,
+//  return the area of the largest rectangle in the histogram.
+//
+//  Example 1:
+//    Input: heights = [2,1,5,6,2,3]
+//    Output: 10
+//
+//  Example 2:
+//    Input: heights = [2,4]
+//    Output: 4
+//----------------------------------------------------------------------------------------------------------------------
+
+package Problem84;
+
+public class LargestRectangleHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+
+        System.out.println(largestRectangleArea(heights));
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        return 0;
+    }
+}
